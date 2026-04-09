1bfc25dbf139169a338daac66341b56733ff0865
alfonsocamacho00089-cloud <alfonsocamacho00089@gmail.com>
2026-04-09 08:22:26 -0400

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,30 +1,47 @@
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
+
+
 firebase.initializeApp({
+
   apiKey: "AIzaSyCaUyhX2iBMl4A5xeKeu_4SeE6HClp4V1s",
+
   authDomain: "real-market-elite-2025.firebaseapp.com",
+
   projectId: "real-market-elite-2025",
+
   storageBucket: "real-market-elite-2025.appspot.com",
+
   messagingSenderId: "226489002778",
+
   appId: "1:226489002778:web:6722d21a9e78b33b5b1aa3"
+
 });
+
+
 
 const messaging = firebase.messaging();
 
 
 
-  // En tu archivo firebase-messaging-sw.js
 messaging.onBackgroundMessage((payload) => {
-  const notificationTitle = payload.notification?.title || "DropisShop"; 
-  
+
+  console.log('Mensaje recibido en segundo plano:', payload);
+
+  const notificationTitle = payload.notification.title || "TuPropina";
+
   const notificationOptions = {
-    body: payload.notification?.body,
-    icon: '/logo.png',   // Asegúrate del "/" al inicio
-    badge: '/badge.png', // Asegúrate del "/" al inicio
-    image: payload.notification?.image || payload.data?.image || '/logo.png' 
+
+    body: payload.notification.body || "Nueva actualización de tasa disponible",
+
+    icon: '/logo.png' 
+
   };
 
-  // Esta línea con el "return" es clave:
-  return self.registration.showNotification(notificationTitle, notificationOptions);
+
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+
 });
