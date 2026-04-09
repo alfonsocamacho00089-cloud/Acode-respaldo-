3af59fd1e1e9613c496031ef049af139e13054e5
DropisApp <250454294+alfonsocamacho00089-cloud@users.noreply.github.com>
2026-04-09 16:51:33 -0400

    Update firebase-config.js

--- Js/firebase-config.js
+++ Js/firebase-config.js
@@ -1,1 +1,13 @@
+// Configuración de Firebase para DropisApp
+const firebaseConfig = {
+    apiKey: "TU_API_KEY",
+    authDomain: "tu-proyecto.firebaseapp.com",
+    projectId: "tu-proyecto",
+    storageBucket: "tu-proyecto.appspot.com",
+    messagingSenderId: "123456789",
+    appId: "tu-app-id"
+};
 
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
